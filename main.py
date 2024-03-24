74f2e6bbbb5af07d63aa11098fc690ab2317396f
Wojtek <wojtekulaski@gmail.com>
2024-03-24 19:05:53 +0100

    revert single page

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import os
+import random
 from flask import Flask, render_template
+from models.walletPosition import WalletPosition
+
+sijaxPath = os.path.join('.', os.path.dirname(__file__), 'static/js/sijax/')
 
 app = Flask(__name__)
+app.config['SIJAX_STATIC_PATH'] = sijaxPath
+app.config['SIJAX_JSON_URI'] = '/static/js/sijax/json2.js'
+
 
 @app.route("/")
 def IndexPage():
@@ -14,9 +22,24 @@ def Welcome():
 def TranactionsPage():
     return render_template('transactions-page/transactions.html')
 
+@app.route('/transactions/add')
+def AddTransaction():
+    return render_template('transactions-page/transactions.html')
+
 @app.route("/wallet-details")
 def WalletSummaryPage():
-    return render_template('wallet-details-page/wallet-details.html')
+    walletPositions = [
+        WalletPosition(7, 2, 10.4),
+        WalletPosition(2, 3, 20.1),
+        WalletPosition(random.randint(1, 10), 1, random.random()),
+    ]
+
+    walletSum = 0;
+
+    for position in walletPositions:
+        walletSum += position.count * position.currentPrice;
+
+    return render_template('wallet-details-page/wallet-details.html', walletPositions = walletPositions, walletSum = walletSum)
 
 @app.route("/user-profile")
 def UserProfilePage():
---
+++ models/walletPosition.py
@@ -0,0 +1,5 @@
+class WalletPosition:
+  def __init__(self, assetId, count, currentPrice):
+    self.assetId = assetId
+    self.count = count
+    self.currentPrice = currentPrice
