9954ef833fc451138527d7a842f281200722f631
Wojtek <wojtekulaski@gmail.com>
2024-03-27 19:18:43 +0100

    calculate wallet details from transactions

--- src/pages/walletDetails.py
+++ src/pages/walletDetails.py
@@ -1,9 +1,29 @@
 
 from flask import Blueprint, render_template
+from sqlalchemy import func, text
+from src import Database
 from src.consts import TEMPLATE_FOLDER
+from src.models.WalletTransaction import WalletTransaction
 
 WalletDetailsBlueprint = Blueprint('walletDetails', __name__, template_folder=TEMPLATE_FOLDER)
 
 @WalletDetailsBlueprint.route("/wallet-details")
 def WalletSummaryPage():
-    return render_template('pages/wallet-details.html')
+    sql_query = """
+    SELECT
+        A.name as assetName,
+        SUM(WT.count) as currentAssetCount,
+        SUM(WT.count * WT.price + WT.fees) as transactionTotalCost
+    FROM
+        wallet_transaction WT
+    JOIN
+        asset A ON WT.assetId = A.id
+    GROUP BY
+        A.id, A.name;
+    """
+
+    walletPositions = Database.session.execute(text(sql_query)).fetchall()
+
+    print(walletPositions);
+
+    return render_template('pages/wallet-details.html', walletPositions=walletPositions)
