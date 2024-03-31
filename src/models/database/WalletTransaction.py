c740cf434a57e41aa99a2e554206e7888699b63f
Wojtek <wojtekulaski@gmail.com>
2024-03-31 01:34:17 +0100

    transactionEdit

--- src/models/database/WalletTransaction.py
+++ src/models/database/WalletTransaction.py
@@ -6,7 +6,7 @@ class WalletTransaction(Database.Model):
   assetId = Database.Column(Database.Integer, Database.ForeignKey('asset.id'))
   asset = Database.relationship("Asset", backref=Database.backref("asset_walletTransaction", uselist=False))
   count = Database.Column(Database.Integer, nullable=False)
-  timestamp = Database.Column(Database.DateTime, nullable=False)
+  timestamp = Database.Column(Database.Date, nullable=False)
   price = Database.Column(Database.Numeric(precision=10, scale=2), nullable=False)
   fees = Database.Column(Database.Numeric(precision=10, scale=2), nullable=True)
 
--- src/pages/transactions.py
+++ src/pages/transactions.py
@@ -8,7 +8,7 @@ from src.models.database.WalletTransaction import WalletTransaction
 
 TransactionsBlueprint = Blueprint('transactions', __name__, template_folder=TEMPLATE_FOLDER)
 
-@TransactionsBlueprint.route('/transactions', methods = ['POST', 'GET'])
+@TransactionsBlueprint.route('/transactions', methods = ['POST', 'GET', 'PUT'])
 def TranactionsPage():
     if request.method == 'POST':
         json = request.get_json();
@@ -23,16 +23,44 @@ def TranactionsPage():
             count=json['count'],
             timestamp=timestampValue, 
             price=json['price'], 
-            fees=json['fee']);
+            fees=json['fees']);
         Database.session.add(transaction)
         Database.session.commit()
 
         return {}
-    else:
+    elif request.method == 'PUT':
+        json = request.get_json();
+
+        print(f"[transactions/edit] json: {json}");
+
+        transactionToUpdate = WalletTransaction.query.get_or_404(json['id'])
+
+
+        timestampValue = datetime.datetime.strptime(json['timestamp'], '%Y-%m-%d')
+
+        transactionToUpdate.assetId = json['assetId']
+        transactionToUpdate.count = json['count']
+        transactionToUpdate.timestamp = timestampValue
+        transactionToUpdate.price = json['price']
+        transactionToUpdate.fees = json['fees']
+
+        Database.session.commit()
+
+        return {}
+    elif request.method == 'GET':
         transactionsToDisplay = WalletTransaction.query.order_by(WalletTransaction.timestamp.desc()).all()
 
         return render_template('pages/transactions.html', transactions = transactionsToDisplay)
+    
 
 @TransactionsBlueprint.route('/transaction-add')
 def TransactionAddPage():
     return render_template('pages/transaction-add.html', assets=Asset.query.all())
+
+@TransactionsBlueprint.route('/transaction-edit/<transactionId>')
+def TransactionEditPage(transactionId):
+    transactionToEdit = WalletTransaction.query.get(transactionId)
+    print(transactionToEdit)
+
+    return render_template('pages/transaction-edit.html', assets=Asset.query.all(), transactionToEdit = transactionToEdit)
+
